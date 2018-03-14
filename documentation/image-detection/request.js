9281fcc4952129b362ce0e8877e94b89cff6c166
Dominic Cabral <dcabral@netra.io>
2018-03-14 16:33:45 -0400

    use variables

--- documentation/image-detection/request.js
+++ documentation/image-detection/request.js
@@ -1,17 +1,19 @@
 'use strict'
 
 const request = require('request');
+const SUBSCRIPTION_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const CALLBACK_URL = 'http://api.webhookinbox.com/i/XXXXXX/'
 
 const options = {
 	url: 'https://api.getnetra.com/image-detection/process/brands',
 	method: 'POST',
 	headers: {
-	  'Ocp-Apim-Subscription-Key': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	  'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
 	  'content-type' : 'application/json'
 	},
 	body: JSON.stringify({
 		"image_url":"https://storage.googleapis.com/netra-public-data/async-image-detection/documentation/api-test-image.jpg",
-		"callback_url":"http://api.webhookinbox.com/i/XXXXXX/"
+		"callback_url": CALLBACK_URL
 	})
 };
 
